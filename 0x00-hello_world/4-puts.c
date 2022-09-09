f9b9a018428ea1cee56fb4ced179eed2eed6db1e
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-08 19:17:35 -0700

    correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <studio.h>
+#include <stdio.h>
+
 /*
- * main - Print
+ * main - print
  *
- * Return: 0 (Success)
+ * return: 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
