ce08cb8d7b06eea6db34b26c4669d6cb2390b2f3
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-13 20:58:59 -0700

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * times_table - prints the 9 times table, starting with 0.
+ * times_table - prints the 9 times table, starting with 0
  */
-void time_table(void)
+void times_table(void)
 {
 	int num, mult, prod;
 
