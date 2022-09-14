98dfd685c8a4b72ab8805d25e2dbf910cc4ac21a
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-14 14:33:28 -0700

    101-natural.c corrected

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main prints count of multiples
+ * main -  prints count of multiples
  * of 3 0r 5 below 1024
  * Return: return 0
  */
 int main(void)
 {
 	int n, sum = 0;
+
 	for (n = 0; n < 1024; n++)
 	{
 		if ((n % 3) == 0 || (n % 5) == 0)
