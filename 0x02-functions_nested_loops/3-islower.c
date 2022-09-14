da467ec3be068b202ced62ace35b20b7b7afa6e8
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-13 20:14:15 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _islower _ function to check lowercase
+ * @c: is the int used for argument
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
