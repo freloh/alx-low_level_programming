390419c7f459cda2056bc88ec86d7fc1249b12db
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-14 19:01:51 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints out first 98
+ * fibonacci suit numbers
+ * Return: return 0
+ */
+int main(void)
+{
+	int inc;
+	unsigned long n = 1, m = 2, o;
+
+	for (inc = 0; inc < 98; inc++)
+	{
+		o = n + m;
+		printf("%lu", o);
+		n = m;
+		m = o;
+
+		if (inc == 98)
+			printf("\n");
+		else
+			printf(", ");
+	}
+	return (0);
+}
