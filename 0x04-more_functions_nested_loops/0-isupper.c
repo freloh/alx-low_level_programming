9d46f4019e96914a938eafb21a3e78b76be14483
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-16 19:02:55 -0700

    correction

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 /**
  * _isupper - checks if a character is uppercase or not
  * @c: character to be tested
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
+else
+return (0);
 }
