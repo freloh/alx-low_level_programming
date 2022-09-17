a8d255bb81112c7bac90dad2863420ca21ab8f8a
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-17 08:49:40 -0700

    0-isupper.c corrected

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,7 +8,7 @@
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
-int isupper(int c)
+int _isupper(int c)
 {
 if ((c >= 'A') && (c <= 'Z'))
 {
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,7 +16,7 @@ void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
 int _putchar(char c);
-int _isipper(int c);
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 
