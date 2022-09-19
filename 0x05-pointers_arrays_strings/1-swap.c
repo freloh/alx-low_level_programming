88a03623bb1b436c80f3a8504211385a43575f3b
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-19 16:04:09 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,15 @@
 
 /**
  * main - swaps the values of two integers
- * @p: 1 pointer
- * @q: 2 pointer
+ * @p : 1 pointer
+ * @q : 2 pointer
  * Return: void
  */
 
 void swap_int(int *p, int *q)
+
 {
+
 	int x = *p;
 	*p = *q;
 	*q = x;
