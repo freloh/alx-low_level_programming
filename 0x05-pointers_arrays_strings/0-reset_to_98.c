811606f4760575ba322596562d51060bb76d9053
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-19 15:49:53 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio>
+
+/**
+ * reset_to_98 - takes a pointer to integer data type and 
+ * updates value
+ * @n: pointer
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
