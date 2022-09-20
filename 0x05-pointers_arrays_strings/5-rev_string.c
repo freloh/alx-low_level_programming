bcf1a26abb8216ea6855dbbfdb2f129db4353457
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-20 03:30:08 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,22 +9,13 @@
 
 void rev_string(char *s)
 {
-	int i, j, k, temp;
+	int i, len, temp;
+	len = strlen(s);
 
-	i = 0;
-	while (s[i] != '\0')
+	for (i = 0; i < len/2; i++)
 	{
-		i++;
-	}
-	k = 0;
-	j = i - 1;
-
-	while (k > j)
-	{
-		temp = s[k];
-		s[k] = s[j];
-		s[j] = temp;
-		k++;
-		j--;
+		temp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = temp;
 	}
 }
