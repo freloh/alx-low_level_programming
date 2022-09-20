67e9ef636115d5ecf2cb91270af9df6be7a2ab5c
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-19 20:51:20 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,9 +8,11 @@
 
 void _puts(char *str)
 {
-       for (str = 0; *str != '\0'; str++)
-       {
-		_putchar(*str);
-       }
+	int i;
+
+	for (i = 0; *str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 	_putchar('\n');
 }
