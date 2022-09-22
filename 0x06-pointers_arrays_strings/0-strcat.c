5e742875f68af1a16fd74dcf3153f6c37982b3ec
Elochukwu Ani <ani.godfrey.elo@gmail.com>
2022-09-22 11:40:45 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,24 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_len = 0;
+	int i;
+	int j;
 
-	while (dest[index++])
-		dest_len++;
+	i = 0;
+	j = 0;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 	return (dest);
 }
